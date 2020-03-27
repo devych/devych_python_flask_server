05010ab45789eb74be7636de5cb75882d609c50c
devych <devych.code@gmail.com>
2020-03-27 17:34:15 +0900

    앱 작성

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,19 @@
-import os
-import json
-
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('env.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     @app.route('/hello')
     def hello():
-        return json({'hello':'world'})
+        return jsonify(hello='world')
 
     return app
 
+
+create_app()
