9ce7520167285992bfdbb6f7d44b8a279b358bb1
devych <devych.code@gmail.com>
2020-04-02 00:42:03 +0900

    ec2 deploy를 위한 setup.py 작성

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # devych_python_flask_server
 
 ##Installation
+
 Requirments:
 - python 3.8
 - Flask
@@ -8,10 +9,12 @@ Requirments:
 
 
 ## Overview
+
 mysql과 연동하여 api를 제공하는 flask server
 
 
 ##Default Setting
+
 env directory에 env.py를 생성하고, db dict를 생성한다.
 
 ```buildoutcfg
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='devych_server',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'PyMySQL',
+        'Jinja2',
+        'requests',
+        'Werkzeug',
+        'urllib3',
+    ],
+)
