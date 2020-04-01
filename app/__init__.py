81e033bd34dc5ccd148b49cec1d79cb28c3d5ee0
devych <devych.code@gmail.com>
2020-04-01 23:19:54 +0900

    사용하지 않는 파일 삭제 및 api method 추가

--- app/__init__.py
+++ app/__init__.py
@@ -7,19 +7,19 @@ print('app.init')
 app = Flask(__name__, instance_relative_config=True)
 
 
-@app.route('/lotto/<num>')
+@app.route('/lotto/<num>', methods=['GET'])
 def get_lotto(num):
     data = Lotto.get_drw_lotto(num)
     return jsonify(data=data)
 
 
-@app.route('/lotto/create/<num>')
+@app.route('/lotto/create/<num>', methods=['GET'])
 def create_lotto(num):
     data = Lotto.create_lotto_num(num)
     return jsonify(data=data)
 
 
-@app.route('/lotto/rank/<boolean>')
+@app.route('/lotto/rank/<boolean>', methods=['GET'])
 def get_lotto_ranking(boolean):
     data = Lotto.ranking_lotto_num(boolean)
     return jsonify(data=data)
