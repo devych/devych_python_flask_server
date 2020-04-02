f40d5ac843295356260344384e1ee216d6c222c1
devych <devych.code@gmail.com>
2020-04-02 23:44:43 +0900

    변화가 있을때 자동으로 reloading 하도록 코드 변경

--- waitress_server.py
+++ waitress_server.py
@@ -1,5 +1,14 @@
+import sys
 from app import app
 from waitress import serve
+import hupper
+
+
+def main(args=sys.argv[1:]):
+    if '--reload' in args:
+        reloader = hupper.start_reloader('waitress_server.main')
+
+    serve(app, host='0.0.0.0', port=5000)
 
 
 serve(app, host='0.0.0.0', port=5000)
